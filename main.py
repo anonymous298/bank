f1e561ae868427808e1dbcb8823711ee46622ff1
anonymous298 <tackletalha@gmail.com>
2023-12-31 16:48:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This is a bank class
 class BankingSystem:
 
     def __init__(self,account_number,holder_name,balance):
@@ -5,10 +6,12 @@ class BankingSystem:
         self.holder_name = holder_name
         self.balance = balance
     
+    # This is a deposit method
     def Deposit(self,amount):
         self.balance += amount
         print(f"{self.account_number} is been deposited {amount} rupees in bank")
 
+    # This is a withdrawal method
     def Withdrawal(self,amount):
         if amount < self.balance:
             self.balance -= amount
@@ -17,16 +20,19 @@ class BankingSystem:
         else:
             print("Insufficent funds!")
 
+    # This method check the balance
     def Balace_check(self):
         print(f"Accout Number: {self.account_number}")
         print(f"Holder Name: {self.holder_name}")
         print(f"Current Balance is {self.balance}")
 
+    # This method gives options to user
     def Atm_Options(self):
         print("(1 for deposit)")
         print("(0 for withdrawal)")
         print("(2 for balance check)")
 
+# This function is use to set a pin in atm
 def pin():
     pin = 1807
     user = int(input("Enter your pin here: "))
@@ -37,6 +43,7 @@ def pin():
     else:
         bank()
 
+# This is the main function
 def bank():
     bank_obj = BankingSystem("123456789","Talha",1000000)
     bank_obj.Atm_Options()
